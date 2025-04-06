71950f29889970b74115b56b2dfeeb5a6c4a08a8
str1f3 <stephen.razon.cnd@gmail.com>
2025-04-06 05:20:31 -1000

    Create arrayListDesign.h

---
+++ DSU/CSC300-DATA_STRUCTURES/ARRAY_BASED_LIST/arrayListDesign.h
@@ -0,0 +1,35 @@
+/*################################################
+# Name: Stephen Razon
+# Student ID:
+# Date: 170741SEP24
+# Filename: arrayListDesign.h
+# Dependency: N/A
+################################################*/
+
+template <class Type>
+class ArrayList
+{
+protected:
+	int selfLength, selfMaxSize;
+	Type* selfList;
+public:
+	ArrayList(int);              //constructor
+	bool isEmpty();
+	bool isFull();
+	int listSize();
+	int maxListSize();
+	const void print();
+	bool isItemAtEqual(int, const Type&);
+	void insertAt(int, const Type&);
+	void insertEnd(const Type&);
+	void removeAt(int);
+	void retrieveAt(int, Type&);
+	void replaceAt(int, const Type&);
+	void clearList();
+	int seqSearch(const Type&);
+	void insert(const Type&);    //without duplication
+	void remove(const Type&);
+	ArrayList(const ArrayList<Type>&);     //copy constructor
+	~ArrayList();
+	const ArrayList<Type>& operator=(const ArrayList<Type>&);
+};
