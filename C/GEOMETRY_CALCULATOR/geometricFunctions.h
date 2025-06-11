ddf5c2fa06e87b9f7ff10410ca6ee804547015a9
str1f3 <stephen.razon.cnd@gmail.com>
2025-06-11 08:43:52 -0400

    Update and rename geometricFunction.h to geometricFunctions.h

---
+++ C/GEOMETRY_CALCULATOR/geometricFunctions.h
@@ -0,0 +1,19 @@
+/*** 2D SHAPES ***/
+  //PERIMETER
+
+/*** Triangle Perimeter Calculator ***/
+float perimeterTriangle(float side1, float side2, float side3){
+    //FORMULA: side1 + side2 + side3
+    return side1 + side2 + side3;
+}
+
+
+//AREA
+
+
+//CIRCUMFERENCE
+
+/*** 3D SHAPES ***/
+//VOLUME
+
+//
