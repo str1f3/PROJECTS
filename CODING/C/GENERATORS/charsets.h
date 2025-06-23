2181bf99f961846bbbc8ad3127b2c4a9ff387c08
str1f3 <stephen.razon.cnd@gmail.com>
2025-06-23 15:06:31 -0400

    Create charsets.h

---
+++ CODING/C/GENERATORS/charsets.h
@@ -0,0 +1,23 @@
+/*######################################################################################
+# Dev: cnd.dev
+# Program Name: N/A
+# Version: 1.0.0
+#  - Major.Minor.Update/BuildNumber
+# Date: 231504JUN25
+# Filename: charsets.h
+# Dependency: N/A
+# Compile Cmd: N/A
+# Synopsis:
+#  - Overview: describes what the program does, how it works, and its key components
+#  - Technical: ...
+######################################################################################*/
+
+#ifndef CHARSETS_H
+#define CHARSETS_H
+
+void populateUppercase(char uppercaseLetters[]);
+void populateLowercase(char lowercaseLetters[]);
+void populateNumbers(char numbers[]);
+void populateSymbols(char symbols[]);
+
+#endif
