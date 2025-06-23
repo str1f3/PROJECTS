bdfe5f4c6336f25cc439412198dd02c63f9c97e8
str1f3 <stephen.razon.cnd@gmail.com>
2025-06-23 15:31:58 -0400

    Update randomPWGenerator.c

--- CODING/C/GENERATORS/randomPWGenerator.c
+++ CODING/C/GENERATORS/randomPWGenerator.c
@@ -11,59 +11,16 @@
 #  - Overview: describes what the program does, how it works, and its key components
 #  - Technical: ...
 ######################################################################################*/
-​
+
 #include <stdio.h>
-​
-void populateUppercase(char uppercaseLetters[]){
-  for (int i = 0; i < 26; i++){
-    uppercaseLetters[i] = i + 65;
-  }
-}
-​
-void populateLowercase(char lowercaseLetters[]){
-  for (int i = 0; i < 26; i++){
-    lowercaseLetters[i] = i + 97;
-  }
-}
-​
-void populateNumbers(char numbers[]){
-  for (int i = 0; i < 10; i++){
-    numbers[i] = i;
-  }
-}
-​
-void populateSymbols(char symbols[]){
-  /*** Total Symbols = 32 ***/
-​
-  //ASCII 33–47: ! " # $ % & ' ( ) * + , - . /
-  for (int i = 0; i < 15; i++){
-    symbols[i] = i + 33;
-  }
-  
-  //ASCII 58–64: : ; < = > ? @
-  for (int i = 0; i < 7; i++){
-    symbols[i + 15] = i + 58;
-  }
-  
-  
-  //ASCII 91–96: [ \ ] ^ _ `
-  for (int i = 0; i < 6; i++){
-    symbols[i + 22] = i + 91;
-  }
-  
-  
-  //ASCII 123–126: { | } ~
-  for (int i = 0; i < 4; i++){
-    symbols[i + 28] = i + 123;
-  } 
-}
-​
+#include "charsets.h"
+
 int main()
 {
-​
+
     char uppercaseLetters[27];
     char lowercaseLetters[27];
-    unsigned char numbers[10];
+    char numbers[10];
     char symbols[33];
     //int arrayLength = (sizeof(uppercaseLetters) / sizeof(int));
     
@@ -89,10 +46,10 @@ int main()
     
     //populate numbers
     populateNumbers(numbers);
-​
+
     //numbers test display
     for (int i = 0; i < 10; i++){
-        printf("%d ", numbers[i]);
+        printf("%c ", numbers[i]);
     }
     
     puts("\n");
@@ -104,6 +61,6 @@ int main()
     for (int i = 0; i < 32; i++){
       printf("%c ", symbols[i]);
     }
-​
+
     return 0;
 }
