9b2a1392ecd8d47fada564606806fa5371419582
str1f3 <stephen.razon.cnd@gmail.com>
2025-06-23 22:31:18 -0400

    Create bitwise.h

---
+++ CODING/C/CALCULATORS/BITWISE_CALCULATOR/bitwise.h
@@ -0,0 +1,26 @@
+/*######################################################################################
+# Dev: cnd.dev
+# Program Name: N/A
+# Version: 1.0.1
+#  - Major.Minor.Update/BuildNumber
+# Date: 181445MAR25
+# Filename: bitwise.h
+# Dependency: N/A
+# Compile Cmd: gcc -m64 -O1 bitwiseCalculator.c -o BitwiseCalculator-v1.0.0-linux-x86-64
+# Synopsis:
+#  - Overview: describes what the program does, how it works, and its key components
+#  - Technical: ...
+######################################################################################*/
+
+#ifndef BITWISE_H
+#define BITWISE_H
+
+//SELECTION MODE
+int selectMode(int* mode);
+
+//GET DATA - AND/OR/XOR
+int getData(int* userInput1, int* userInput2);
+
+int decimalToBinary(int userInput);
+
+#endif
