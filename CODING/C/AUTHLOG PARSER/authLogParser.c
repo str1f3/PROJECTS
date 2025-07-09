868ec7964b42dc863e2ca28a87782b5671f2a1c1
Strife <stephen.razon.cnd@gmail.com>
2025-07-08 20:58:20 -0400

    updated authLogParser.c and binary

--- CODING/C/AUTHLOG PARSER/authLogParser.c
+++ CODING/C/AUTHLOG PARSER/authLogParser.c
@@ -15,92 +15,146 @@
 #    system to distinguish between mistyped and malicious logins.
 #  - Technical: ...
 ######################################################################################*/
-​
+
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
-​
-int selectMode(int *mode) {
+
+/*int selectMode(int *mode) {
   bool selection = true;
   while (selection) {
     printf("MODE: ");
-    if (scanf("%d", mode) == 1 && (*mode == 1 || *mode == 2)) {
+    if (scanf("%d", mode) == 1 && (*mode == 1 || *mode == 2) ) {
       break;
     }
     while (getchar() != '\n');  // Clear invalid input
   }
   return 0;
+}*/
+
+//mode selector
+int modeSelector(void){
+  int mode;
+  int inputStatus;
+
+  while (1){
+    printf("mode: ");
+    inputStatus = scanf("%d", &mode);
+    //if input is successfully read...clear the rest of the line including newline
+    if (inputStatus == 1){
+      while (getchar() != '\n');
+        return mode;
+    } 
+    else {
+      printf("Invalid input. Select a number.\n");
+      while (getchar() != '\n');
+    }
+  }
 }
-​
-FILE* openFile(){
+
+FILE* openAuthLogFile(){
   int flag = 1;
   char filePath[255] = "auth.log";   //full path /var/log/auth.log
-  FILE* file = fopen(filePath, "r");
-  return file;
+  FILE* authLogFile = fopen(filePath, "r");
+  return authLogFile;
 }
-​
+
+//this passwd file is just a helper for identifying usernames that aren't in the system
+//when a brute force attack is occurring, actors are typically spraying a bunch of usernames and passwords in a short amount of time
+//for password spraying, it is the password that is being sprayed a bunch of times against a single username
+FILE* openPasswdFile(){
+  int flag = 1;
+  char filePath[255] = "passwd";  //full path /etc/passwd
+  FILE * passwdFile = fopen(filePath, "r");
+  return passwdFile;
+}
+
 int main(int argc, char *argv[])
 {
   int mode = 0;
-  unsigned int totalFailedLogins = 0;
-  unsigned int totalSuccessfulLogins = 0;
-  unsigned char lineReadBuffer = 255;
+  int totalFailedLogins = 0;
+  int totalSuccessfulLogins = 0;
+  int lineReadBuffer = 255;
   char readLines[lineReadBuffer];
-  //char delimiter[] = " ";
-  //char *token = NULL;
   char failedLoginsMarker[] = "Failed password for";
   char successfulLoginsMarker[] = "Accepted password for";
-​
-​
+
+  //char delimiter[] = " ";
+  //char *token = NULL;
+  //
+  // Pointer to hold each extracted word (token)
+  //char *token = strtok(str, delimiter);
+
+//----------------------------------------------------
+
   puts("Select Mode: ");
   //puts("1. Brute Force Detection: ");
   //puts("2. Command Injection Detection: ");
   puts("1. Failed Logins");
   puts("2. Successful Logins");
-​
-  selectMode(&mode);
-​
+  puts("3. Valid Users");
+
+  mode = modeSelector();
+
   //INPUT
-  FILE* file = openFile();
-​
-  if (file == NULL){
+  FILE* authLogFile = openAuthLogFile();
+  FILE* passwdFile = openPasswdFile();
+
+  if (authLogFile == NULL || passwdFile == NULL){
     puts("Error opening file.\n");
     puts("Rerun the program to continue...");
   }
   else{
-    puts("File Successfully Opened");
-​
-    switch (mode) {
-      case 1:{
-        puts("******* BRUTE FORCE DETECTION MODE *******");
-        //PROCESS: 1.FAILED LOGINS
-        while(fgets(readLines, sizeof(readLines), file)){
-          if(strstr(readLines, failedLoginsMarker)){
-            printf("%s", readLines);
-            totalFailedLogins++;
-          }
+    puts("Files Successfully Opened");
+
+    if (mode == 1){	  
+	  //PROCESS: 1.DISPLAY FAILED LOGINS
+	  while(fgets(readLines, sizeof(readLines), authLogFile)){
+	    if (strstr(readLines, failedLoginsMarker)){
+		  printf("%s", readLines);
+		  totalFailedLogins++;
+		}
+	  }
+	  printf("\nTotal Failed Logins: %d", totalFailedLogins);
+	}
+	else if (mode == 2){
+	  //PROCESS: 2.DISPLAY SUCCESSFUL LOGINS
+	  while(fgets(readLines, sizeof(readLines), authLogFile)){
+	    if (strstr(readLines, successfulLoginsMarker)){
+		  printf("%s", readLines);
+		  totalSuccessfulLogins++;
+		}
+	  }
+	  printf("\nTotal Successful Logins: %d", totalSuccessfulLogins);
+	}
+	else if (mode == 3){
+	  //GET ONLY THE 1st word (usernames) in the passwd file (fullpath /etc/passwd) - 100 usernames total (for now)
+	  char usernameDelimiter[] = ":";   //tokenize /etc/passwd
+	  char passwdUsernames[100][100];
+	  char *token = NULL;
+	  int userIndex = 0;
+	  
+	  while(fgets(readLines, sizeof(readLines), passwdFile) && userIndex < 100){
+	    // Pointer to hold each extracted word (token)
+	    token = strtok(readLines, usernameDelimiter);    //this grabs the usernames (first column)
+	  
+	    // Loop to extract and print all tokens
+        if (token != NULL) {
+		  strncpy(passwdUsernames[userIndex], token, sizeof(passwdUsernames[userIndex]) - 1);
+		  passwdUsernames[userIndex][sizeof(passwdUsernames[userIndex]) - 1] = '\0';  // null terminate each usernames
+          userIndex++;
         }
-        printf("\nTotal Failed Logins: %d", totalFailedLogins);
-        break;
+	  }	
+      for (int i = 0; i < userIndex; i++) {
+        printf("Valid User [%d]: %s\n", i, passwdUsernames[i]);
       }
-      case 2:{
-        puts("******* BRUTE FORCE DETECTION MODE *******");
-        //PROCESS: 2.SUCCESSFUL LOGINS
-        while(fgets(readLines, sizeof(readLines), file)){
-          if(strstr(readLines, successfulLoginsMarker)){
-            printf("%s", readLines);
-            totalSuccessfulLogins++;
-          }
-        }
-        printf("\nTotal Successful Logins: %d", totalSuccessfulLogins);
-        break;
-      }
-      default:{
-        puts("Invalid Selection...rerun the program again to continue.");
-      }
-    }
+	}
+	else{
+	  puts("Invalid Selection...rerun the program again to continue.");
+	}
   }
-  fclose(file);
-​
+  fclose(authLogFile);
+  fclose(passwdFile);
+
   return 0;
 }
