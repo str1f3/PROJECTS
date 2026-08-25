e23723b271805fce1f27bc0d453a5417517fe43a
Strife <stephen.razon.cnd@gmail.com>
2026-08-24 23:10:23 -0400

    nixEnum project

---
+++ CODING/C/NIX_ENUM/nixEnum.c
@@ -0,0 +1,20 @@
+/*######################################################################################
+# Dev: cnd.dev
+# Program Name: nixEnum
+# Version: 0.1.0
+# Date: 242252AUG26
+# Source: nixEnum.c
+# Dependency: N/A
+# Compile Cmd: gcc -m64 -O1 nixEnum.c -o nixEnum
+# Synopsis:
+#  - Overview: A Linux host enumeration utility written in C to support
+#    Offensive Cyber Operations (OCO) by automatically collecting and
+#    displaying system information relevant to host assessment. The utility
+#    is intended to reduce the need for manual enumeration by programmatically
+#    gathering information about the target system, its users, processes,
+#    resources, networking, configuration, and security posture.
+#  - Technical: Developed as a systems-programming project to explore Linux
+#    APIs, system calls, system resources, process information, filesystem
+#    interfaces, and the interaction between user-space applications and the
+#    operating system.
+######################################################################################*/
